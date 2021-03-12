36b35aa6fa2dd470f10dc553f57976762f50756a
tanxin <tanxin_java@163.com>
2021-03-12 16:23:12 +0800

    pdf

---
+++ demo-mp-server/src/main/java/com/dome/mp/server/utils/PdfUtils.java
@@ -0,0 +1,58 @@
+package com.dome.mp.server.utils;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.BaseFont;
+import org.xhtmlrenderer.pdf.ITextFontResolver;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * <p>description</p>
+ *
+ * @author TanXin
+ * @date 2021/3/12 10:37
+ */
+public class PdfUtils {
+
+    /**
+     * 通过html的字符串转pdf
+     *
+     * @param out
+     * @param html
+     * @throws IOException
+     * @throws DocumentException
+     */
+    public static void createPdfByHtml(OutputStream out, String html) throws IOException, DocumentException {
+        ITextRenderer renderer = new ITextRenderer();
+        renderer.setDocumentFromString(html);
+        // 解决中文支持问题
+        ITextFontResolver fontResolver = renderer.getFontResolver();
+        fontResolver.addFont("c:/Windows/Fonts/simsun.ttc", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+        renderer.layout();
+        renderer.createPDF(out);
+    }
+
+    /**
+     * 通过html的文件路径转pdf
+     *
+     * @param out
+     * @param htmlFilePath
+     * @throws IOException
+     * @throws DocumentException
+     */
+    public static void createPdfByUrl(OutputStream out, String htmlFilePath) throws IOException, DocumentException {
+        ITextRenderer renderer = new ITextRenderer();
+        String url = new File(htmlFilePath).toURI().toURL().toString();
+        renderer.setDocument(url);
+        // 解决中文支持问题
+        ITextFontResolver fontResolver = renderer.getFontResolver();
+        fontResolver.addFont("c:/Windows/Fonts/simsun.ttc", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+        //解决图片的相对路径问题
+        //renderer.getSharedContext().setBaseURL("http://localhost:8080");//file:/e:/
+        renderer.layout();
+        renderer.createPDF(out);
+    }
+}
---
+++ demo-mp-server/src/main/java/com/dome/mp/server/web/controller/PdfCreateController.java
@@ -0,0 +1,37 @@
+package com.dome.mp.server.web.controller;
+
+import com.dome.mp.server.utils.PdfUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.OutputStream;
+
+/**
+ * <p>description</p>
+ *
+ * @author TanXin
+ * @date 2021/3/12 10:07
+ */
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("/demo/pdf")
+public class PdfCreateController {
+
+
+    @GetMapping("/test")
+    public void test(HttpServletRequest request, HttpServletResponse response, String path) {
+        try {
+            OutputStream outputStream = response.getOutputStream();
+            PdfUtils.createPdfByUrl(outputStream, path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
